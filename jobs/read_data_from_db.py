c47248c82203cbaa0561e4b5676a1c1e9a69bfb9
rxmi-bkd <boukadidarami0@gmail.com>
2024-05-08 17:04:24 +0200

    replace " by ' in code

--- jobs/read_data_from_db.py
+++ jobs/read_data_from_db.py
@@ -2,9 +2,9 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('read_data_from_db').getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:mysql://database:3306/olist").option("driver",
-                                                                                 "com.mysql.cj.jdbc.Driver").option(
-    "dbtable", "users").option("user", "admin").option("password", "admin").load()
+df = spark.read.format('jdbc').option('url', 'jdbc:mysql://database:3306/olist').option('driver',
+                                                                                 'com.mysql.cj.jdbc.Driver').option(
+    'dbtable', 'users').option('user', 'admin').option('password', 'admin').load()
 
 df.show()
 
